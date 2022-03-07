c04808dfd41ae1512312021f8d3d0023d6803e2a
Novercae <98193343+Novercae@users.noreply.github.com>
2022-03-07 15:22:18 -0300

    Update code.py

--- code.py
+++ code.py
@@ -1,31 +1,32 @@
 text = "bbbbbbbbbbbbbbbaaaaaaaaaaaaaacccdddd".upper()
 
+
 def codificar(valor):
     codi = {}
     index = 0
     string = []
-    for x in valor:
+    for x in valor:     # here is gonna count the letters with the dict
         if x in codi:
             codi[x] += 1
         else:
             codi[x] = 1
 
-    keys_v = list(codi.keys())
-    values_v = list(codi.values())
+    keys_v = list(codi.keys())      # take the number of letters and pass to list
+    values_v = list(codi.values())      # take the letters and pass to list
 
     for x in values_v:
-        if int(x) >= 9:
+        if int(x) >= 9:   # here is gonna check if there more than 9 letters
             string.append('9')
             string.append(str(keys_v[index]))
             string.append(str(x - 9))
             string.append(keys_v[index])
             index += 1
-        if int(x) < 9:
+        if int(x) < 9:  # here if there less than 9 letters
             string.append(str(x))
             string.append(keys_v[index])
             index += 1
     s = ''.join((string))
     return s
 
-  
- print(codificar(text))
+
+print(codificar(text))
